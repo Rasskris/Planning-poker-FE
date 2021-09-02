03ba92ca0a41385a2a1faf2f7490ccf30e45b51a
elenaniksy <elfhelen@gmail.com>
2021-09-02 13:27:55 +0300

    feat: add basic styles module to app.tsx

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import classes from './App.module.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      {/*  test header in component to show, that styles added*/}
+      <h1 className={classes.header}>Pointing Poker</h1>
     </div>
   );
 }
