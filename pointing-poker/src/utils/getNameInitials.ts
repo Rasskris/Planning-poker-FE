04b74a9dd4af6c8b5120a8d3eb22c4fc65665b30
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-04 16:53:22 +0300

    feat: add util function

---
+++ pointing-poker/src/utils/getNameInitials.ts
@@ -0,0 +1,8 @@
+const getNameInitials = (firstName: string, lastName?: string) => {
+  const firstLetter = `${firstName[0]}`;
+  const secondLetter = lastName ? `${lastName[0]}` : '';
+
+  return `${firstLetter}${secondLetter}`;
+};
+
+export default getNameInitials;
