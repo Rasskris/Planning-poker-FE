f9a1fb59183479a599462eec1b227ccda73cb79c
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-05 19:02:08 +0300

    feat: add Button component and styles

---
+++ pointing-poker/src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  onClick: Function;
+  children: Node;
+  disabled: boolean;
+  colorButton: string; //dark or light
+}
+
+const Button = (props: ButtonProps) => {
+  const { onClick, children, disabled, colorButton, ...attr } = props;
+  const classes: Array<String> = [styles.button, styles[`button_${colorButton}`]];
+
+  return (
+    <button onClick={() => onClick()} disabled={disabled} className={classes.join(' ')} {...attr}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  onClick: () => {},
+  children: 'Default button',
+  disabled: false,
+  colorButton: 'light',
+};
+
+export default Button;
