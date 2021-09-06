2f9fcbee8a14f79e62d889eb4e9cdc86d5cca0b0
elenaniksy <elfhelen@gmail.com>
2021-09-06 21:06:14 +0300

    feat: add test for footer component

---
+++ pointing-poker/src/components/Footer/Footer.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+import classes from './Footer.module.scss';
+
+describe('Footer', () => {
+  it('renders Footer component', () => {
+    render(<Footer />);
+    screen.debug();
+  });
+
+  it('renders logo block', () => {
+    const { container } = render(<a className={classes.logo} href={'https://rs.school/react/'} />);
+    expect(container.firstChild).toBeInTheDocument();
+  });
+
+  it('renders copyright block', () => {
+    const { getByText } = render(<Footer />);
+    const copyrightElement = getByText(/made by/i);
+    expect(copyrightElement).toBeInTheDocument();
+    expect(copyrightElement.childElementCount).toBe(3);
+  });
+});
