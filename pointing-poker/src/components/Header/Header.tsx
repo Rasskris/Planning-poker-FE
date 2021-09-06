955b54775cee8f3d289c178dd93dd6d1a7d3d85c
elenaniksy <elfhelen@gmail.com>
2021-09-06 14:45:43 +0300

    fix: fix styles structure

--- pointing-poker/src/components/Header/Header.tsx
+++ pointing-poker/src/components/Header/Header.tsx
@@ -4,17 +4,17 @@ import classes from './Header.module.scss';
 const Header: React.FC = (): JSX.Element => {
   return (
     <div className={classes.header}>
-      <div className={classes.header__wrapper}>
-        <div className={classes.header__background}>
-          <div className={classes.header__background_darkBlue} />
-          <div className={classes.header__background_blue} />
+      <div className={classes.wrapper}>
+        <div className={classes.background}>
+          <div className={classes.background_darkBlue} />
+          <div className={classes.background_blue} />
         </div>
-        <div className={classes.header__icons}>
-          <div className={classes.header__icons__logo}>
-            <div className={classes.header__icons__logo_icon} />
+        <div className={classes.icons}>
+          <div className={classes.icons__logo}>
+            <div className={classes.icons__logo_icon} />
           </div>
-          <div className={classes.header__icons__chat}>
-            <div className={classes.header__icons__chat_icon} />
+          <div className={classes.icons__chat}>
+            <div className={classes.icons__chat_icon} />
           </div>
         </div>
       </div>
