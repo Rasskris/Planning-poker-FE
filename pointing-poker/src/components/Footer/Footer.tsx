2462137f66babac8a0113500906ac9444e15ff3d
elenaniksy <elfhelen@gmail.com>
2021-09-06 19:06:51 +0300

    feat: add basic style of footer wrapper

---
+++ pointing-poker/src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Footer.module.scss';
+
+const Footer: React.FC = (): JSX.Element => {
+  return <div className={classes.wrapper}></div>;
+};
+
+export default Footer;
