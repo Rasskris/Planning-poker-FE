14db7e853853c7a9a21b33cb4dce57f1f7ec3aa6
elenaniksy <elfhelen@gmail.com>
2021-09-06 19:39:04 +0300

    feat: add rs logo to footer component

--- pointing-poker/src/components/Footer/Footer.tsx
+++ pointing-poker/src/components/Footer/Footer.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import classes from './Footer.module.scss';
 
 const Footer: React.FC = (): JSX.Element => {
-  return <div className={classes.wrapper}></div>;
+  return (
+    <div className={classes.footer}>
+      <div className={classes.wrapper}>
+        <div className={classes.logo}>
+          <div className={classes.logo_icon} />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
