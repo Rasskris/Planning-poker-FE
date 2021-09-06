fb14421c45cea7d8dd8e42fad5d1d078f5133cae
elenaniksy <elfhelen@gmail.com>
2021-09-06 13:02:39 +0300

    refactor: reorganized component structure

--- pointing-poker/src/components/Header/Header.tsx
+++ pointing-poker/src/components/Header/Header.tsx
@@ -5,11 +5,18 @@ const Header: React.FC = () => {
   return (
     <div className={classes.header}>
       <div className={classes.header__wrapper}>
-        <div className={classes.header__logo}>
-          <div className={classes.header__logo__icon} />
+        <div className={classes.header__background}>
+          <div className={classes.header__background_darkBlue} />
+          <div className={classes.header__background_blue} />
         </div>
-        <div className={classes.header__wrapper_darkBlue} />
-        <div className={classes.header__wrapper_blue} />
+        <div className={classes.header__icons}>
+          <div className={classes.header__icons__logo}>
+            <div className={classes.header__icons__logo_icon} />
+          </div>
+          <div className={classes.header__icons__chat}>
+            <div className={classes.header__icons__chat_icon} />
+          </div>
+        </div>
       </div>
     </div>
   );
