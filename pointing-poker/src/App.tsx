7ca8ab0b833062768d6537c7ed7f1afa5fbc74d3
elenaniksy <elfhelen@gmail.com>
2021-09-06 19:07:15 +0300

    feat: import footer component to app

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './App.module.scss';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
   return (
     <div className={classes.app}>
       <Header />
+      <Footer />
     </div>
   );
 };
