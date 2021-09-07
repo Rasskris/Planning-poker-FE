9d88f7c0086f2cd5e84a6f506142a1fc34a1e632
elenaniksy <elfhelen@gmail.com>
2021-09-07 12:31:22 +0300

    feat: add styles

--- pointing-poker/src/components/KickForm/KickForm.tsx
+++ pointing-poker/src/components/KickForm/KickForm.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './KickForm.module.scss';
 
 const KickForm: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <div></div>
+    <div className={classes.logo}>
+      <div className={classes.logo_icon}></div>
     </div>
   );
 };
