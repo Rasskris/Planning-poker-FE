f46bcaf4a20ee8d2f3dea6b39b024b7d9163c7b3
elenaniksy <elfhelen@gmail.com>
2021-09-07 12:09:43 +0300

    refactor: changed component to change automatically layout to row or to column, receiving layout from props.

--- pointing-poker/src/components/Input/Input.tsx
+++ pointing-poker/src/components/Input/Input.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import classes from './Input.module.scss';
+import { IClasses } from '../../interfaces/IClasses';
 
 interface IPropsInput {
+  layout: string;
   type: string;
   label: string;
   value: string;
@@ -15,9 +17,16 @@ interface IPropsInput {
 const Input: React.FC<IPropsInput> = (props: IPropsInput): JSX.Element => {
   const inputType: string = props.type || 'text';
   const htmlFor: string = `${props.type}-${Math.random()}`;
+
+  const incomingClasses: IClasses = { ...classes };
+  const layout = props.layout === 'row' ? incomingClasses.row : incomingClasses.column;
+  const cls: string[] = [layout];
+
   return (
-    <div className={classes.wrapper}>
-      <label htmlFor={htmlFor}>{props.label}</label>
+    <div className={cls.join(' ')}>
+      <label className={classes.label} htmlFor={htmlFor}>
+        {props.label}
+      </label>
       <input className={classes.input} type={inputType} id={htmlFor} value={props.value} onChange={props.onChange} />
     </div>
   );
