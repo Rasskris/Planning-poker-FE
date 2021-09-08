e159cab9a880a98e6a78fc327b3d08e926ffeabd
elenaniksy <elfhelen@gmail.com>
2021-09-08 12:45:20 +0300

    refactor: changed uniqueId of inputs label from math.random to lodash.uniqueid

--- pointing-poker/src/components/Input/Input.tsx
+++ pointing-poker/src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Input.module.scss';
 import { IClasses } from '../../interfaces/IClasses';
+import _uniqueId from 'lodash/uniqueId';
 
 interface IPropsInput {
   layout: string;
@@ -16,20 +17,19 @@ interface IPropsInput {
 
 const Input: React.FC<IPropsInput> = (props: IPropsInput): JSX.Element => {
   const inputType: string = props.type || 'text';
-  const htmlFor: string = `${props.type}-${Math.random()}`;
-
+  const [uniqueId] = useState(_uniqueId(props.type));
   const incomingClasses: IClasses = { ...classes };
   const layout = props.layout === 'row' ? incomingClasses.row : incomingClasses.column;
   const cls: string[] = [layout];
 
   return (
     <div className={cls.join(' ')}>
-      <label className={classes.label} htmlFor={htmlFor}>
+      <label className={classes.label} htmlFor={uniqueId}>
         {props.label}
       </label>
-      <input className={classes.input} type={inputType} id={htmlFor} value={props.value} onChange={props.onChange} />
+      <input className={classes.input} type={inputType} id={uniqueId} value={props.value} onChange={props.onChange} />
     </div>
   );
 };
 
-export default Input;
+export { Input };
