a298c1606f7ff0544598276e3bb344502e681fa9
elenaniksy <elfhelen@gmail.com>
2021-09-08 12:54:40 +0300

    refactor: fixed component import

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './App.module.scss';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import { Footer } from './components/Footer/Footer';
 
 const App: React.FC = () => {
   return (
