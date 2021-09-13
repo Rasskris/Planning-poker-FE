8e19397e9888835e446bf52bf2ef93eea573716f
elenaniksy <elfhelen@gmail.com>
2021-09-13 17:45:48 +0300

    feat: add basic component layout

--- pointing-poker/src/components/Form/Form.tsx
+++ pointing-poker/src/components/Form/Form.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../store/slices/userSlice';
+import classes from './Form.module.scss';
+import { Input } from '../index';
+import { InputLayoutTypes } from '../../interfaces/InputLayoutTypes';
+import { RootState } from '../../store/store';
 
 const Form: React.FC = (): JSX.Element => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state: RootState) => state.user);
+  const onFormSubmit = () => {
+    console.log('form submit');
+  };
   return (
-    <div>
-      <div></div>
+    <div className={classes.wrapper}>
+      <form className={classes.form} onSubmit={onFormSubmit}>
+        {Object.keys(currentUser.user).map((item, index) => {
+          return item === 'image' ? (
+            <Input
+              key={item}
+              layout={InputLayoutTypes.column}
+              type={'file'}
+              label={item}
+              value={''}
+              touched={false}
+              validate={false}
+              shouldValidate={false}
+              onChangeInputHandler={() => {
+                console.log(item);
+              }}
+            />
+          ) : (
+            <Input
+              key={item}
+              layout={InputLayoutTypes.column}
+              type={'text'}
+              label={item}
+              value={''}
+              touched={false}
+              validate={false}
+              shouldValidate={false}
+              onChangeInputHandler={() => {
+                console.log(item);
+              }}
+            />
+          );
+        })}
+      </form>
     </div>
   );
 };
