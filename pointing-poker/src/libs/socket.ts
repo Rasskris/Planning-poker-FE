556d55e6945bfe4761eacc4a3c2687de6f5fddd3
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-13 17:27:20 +0300

    PP-06: add socket events and emits

---
+++ pointing-poker/src/libs/socket.ts
@@ -0,0 +1,41 @@
+import { Dispatch } from '@reduxjs/toolkit';
+import { io, Socket } from 'socket.io-client';
+import { addMessage, addIssue, deleteIssue, editIssue } from '../store/slices';
+import { URL } from '../constants';
+
+export const initSocket = (userId: string, gameId: string, dispatch: Dispatch): Socket => {
+  const socket = io(URL, {
+    transports: ['websocket', 'polling'],
+    auth: {
+      userId: userId,
+    },
+  });
+
+  socket.emit('joinToRoom', gameId);
+
+  socket.on('connect', () => {
+    console.log(`socket connected: ${socket.id}`);
+  });
+
+  socket.on('message', ({ message }) => {
+    dispatch({ type: addMessage.fulfilled.type, payload: message });
+  });
+
+  socket.on('issue', ({ issue }) => {
+    dispatch({ type: addIssue.fulfilled.type, payload: issue });
+  });
+
+  socket.on('issueEdit', ({ issue }) => {
+    dispatch({ type: editIssue.fulfilled.type, payload: issue });
+  });
+
+  socket.on('issueDelete', ({ issueId }) => {
+    dispatch({ type: deleteIssue.fulfilled.type, payload: issueId });
+  });
+
+  socket.on('disconnect', reason => {
+    console.log(`socked disconnected: ${reason}`);
+  });
+
+  return socket;
+};
