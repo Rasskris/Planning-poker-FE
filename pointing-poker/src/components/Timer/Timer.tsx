f2592a2d0a74acf3fda329d88e1dcedd9f597e55
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-14 01:37:05 +0300

    feat: added editSetting property in Timer component, added minutes and seconds fields handlers

--- pointing-poker/src/components/Timer/Timer.tsx
+++ pointing-poker/src/components/Timer/Timer.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import styles from './Timer.module.scss';
 
 interface TimerProps {
   minutes: number;
   seconds: number;
+  editSetting: boolean;
+  onChangeMinutes: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangeSeconds: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Timer = ({ minutes, seconds }: TimerProps) => (
+const Timer = ({ minutes, seconds, editSetting, onChangeMinutes, onChangeSeconds }: TimerProps) => (
   <div className={styles.timer}>
     <div className={styles.timer__minutes}>
       <p>minutes</p>
-      <p> {minutes}</p>
+      {editSetting ? <input type="number" value={minutes} onChange={onChangeMinutes} /> : <p> {minutes}</p>}
     </div>
     <div className={styles.timer__seconds}>
       <p>seconds</p>
       <p>
         <span>:</span>
-        {seconds < 10 ? `0${seconds}` : seconds}
+        {editSetting ? (
+          <input type="number" value={seconds} onChange={onChangeSeconds} />
+        ) : (
+          <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
+        )}
       </p>
     </div>
   </div>
--- pointing-poker/src/components/Timer/TimerContainer.tsx
+++ pointing-poker/src/components/Timer/TimerContainer.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { useState, useEffect } from 'react';
 import { Timer } from './Timer';
 
 //https://stackoverflow.com/questions/40885923/countdown-timer-in-react
 
 const START_COUNTDOWN_SECONDS = 59;
-const DEFAULT_START_MINUTES = 1;
-const DEFAULT_START_SECONDS = 1;
+const DEFAULT_START_MINUTES = 0;
+const DEFAULT_START_SECONDS = 0;
 
 interface TimerContainerProps {
   initialMinute: number;
   initialSeconds: number;
   timerStarted: boolean;
+  editSetting: boolean;
+  onChangeTimer?: (arg0: {}) => void;
 }
 
 const TimerContainer = (props: TimerContainerProps) => {
-  const { initialMinute, initialSeconds, timerStarted } = props;
-  const [minutes, setMinutes] = useState(initialMinute);
-  const [seconds, setSeconds] = useState(initialSeconds);
+  const { initialMinute, initialSeconds, timerStarted, editSetting, onChangeTimer } = props;
+  const [minutes, setMinutes] = useState<number>(initialMinute);
+  const [seconds, setSeconds] = useState<number>(initialSeconds);
+
+  const handleChangeMinutes = (event: ChangeEvent<HTMLInputElement>) => {
+    //TODO: add validation
+    const { value } = event.target;
+    setMinutes(+value);
+    if (onChangeTimer) onChangeTimer({ minutes, seconds });
+  };
+
+  const handleChangeSeconds = (event: ChangeEvent<HTMLInputElement>) => {
+    //TODO: add validation
+    const { value } = event.target;
+    setSeconds(+value);
+    if (onChangeTimer) onChangeTimer({ minutes, seconds });
+  };
+
   useEffect(() => {
-    if (!timerStarted) return;
+    if (!timerStarted || editSetting) return;
     let myInterval = setInterval(() => {
       if (seconds > 0) {
         setSeconds(seconds - 1);
@@ -38,13 +55,22 @@ const TimerContainer = (props: TimerContainerProps) => {
     };
   });
 
-  return <Timer minutes={minutes} seconds={seconds} />;
+  return (
+    <Timer
+      minutes={minutes}
+      seconds={seconds}
+      editSetting={editSetting}
+      onChangeMinutes={handleChangeMinutes}
+      onChangeSeconds={handleChangeSeconds}
+    />
+  );
 };
 
 TimerContainer.defaultProps = {
   initialMinute: DEFAULT_START_MINUTES,
   initialSeconds: DEFAULT_START_SECONDS,
   timerStarted: false,
+  editSetting: false,
 };
 
 export { TimerContainer };
