0a6bbd776e47da5c322ed62b7c926f29a122425b
elenaniksy <elfhelen@gmail.com>
2021-09-14 21:25:56 +0300

    fix: fix mainPage component layout

--- pointing-poker/src/components/MainPage/MainPage.tsx
+++ pointing-poker/src/components/MainPage/MainPage.tsx
@@ -1,42 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './MainPage.module.scss';
 import { useSelector } from 'react-redux';
-import { Button, Input, BackDrop, Switcher, Form } from '../index';
+import { Button, Input, BackDrop, Form } from '../index';
 import { InputLayoutTypes } from '../../interfaces/InputLayoutTypes';
 import { RootState } from '../../store/store';
 
+const CONNECT = {
+  id: 'connect',
+};
+
+const CREATE = {
+  id: 'create',
+};
+
 const MainPage: React.FC = (): JSX.Element => {
-  const currentState = useSelector((state: RootState) => state.user.user);
+  const currentState = useSelector((state: RootState) => state.user);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onModalHandler = (event: React.MouseEvent): void => {
+    const target = event.target as HTMLButtonElement;
+    const id = target.id;
+
+    if (!isModalOpen) {
+      setIsModalOpen(!isModalOpen);
+    }
+
+    switch (id) {
+      case 'connect':
+        break;
+
+      case 'create':
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const onModalCloseHandler = () => {
+    if (isModalOpen) {
+      setIsModalOpen(!isModalOpen);
+    }
+  };
+
   return (
     <div className={classes.wrapper}>
-      <BackDrop
-        isBackDropOpen={true}
-        titleModal={'Connect to lobby'}
-        children={[
-          <Switcher
-            switchState={currentState.observer}
-            name="formSwitcher"
-            children={'Connect as Observer'}
-            onClick={() => {
-              console.log('switcher');
-            }}
-          />,
-          <Form />,
-        ]}
-      />
+      {isModalOpen ? (
+        <BackDrop
+          isBackDropOpen={true}
+          titleModal={'Connect to lobby'}
+          children={<Form onModalCloseHandler={onModalCloseHandler} />}
+        />
+      ) : null}
       <div className={classes.logo}>
         <div className={classes.logo_icon} />
       </div>
       <h1 className={classes.header}>Start your planning:</h1>
       <div className={classes.create}>
         <p>Create session:</p>
-        <Button
-          children="Start new game"
-          colorButton="dark"
-          onClick={() => {
-            console.log('create');
-          }}
-        />
+        <Button children="Start new game" colorButton="dark" onClick={onModalHandler} {...CREATE} />
       </div>
       <p className={classes.header}>OR:</p>
       <div className={classes.connect}>
@@ -45,20 +67,11 @@ const MainPage: React.FC = (): JSX.Element => {
           type={'text'}
           label={'Connect to lobby by URL:'}
           value={''}
-          touched={false}
-          validate={true}
-          shouldValidate={true}
           onChangeInputHandler={() => {
-            console.log('input');
+            console.log('input url');
           }}
         />
-        <Button
-          children="Connect"
-          colorButton="dark"
-          onClick={() => {
-            console.log('connect');
-          }}
-        />
+        <Button children="Connect" colorButton="dark" onClick={onModalHandler} {...CONNECT} />
       </div>
     </div>
   );
