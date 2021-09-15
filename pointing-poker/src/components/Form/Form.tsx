e5838543f0e594addbf04f48f41fa1a458d4003b
elenaniksy <elfhelen@gmail.com>
2021-09-15 15:20:50 +0300

    feat: add validation of form inputs

--- pointing-poker/src/components/Form/Form.tsx
+++ pointing-poker/src/components/Form/Form.tsx
@@ -5,7 +5,6 @@ import classes from './Form.module.scss';
 import { Input, Button, Switcher } from '../index';
 import { InputLayoutTypes } from '../../interfaces/InputLayoutTypes';
 import { RootState } from '../../store/store';
-import { UserRole } from '../../interfaces/UserRole';
 
 interface IUserField {
   text: string;
@@ -30,6 +29,9 @@ const USER_INIT: { firstName: IUserField; lastName: IUserField; jobPosition: IUs
     type: 'file',
   },
 };
+const BUTTON_SUBMIT_TYPE = {
+  type: 'submit',
+};
 
 interface IFormProps {
   onModalCloseHandler: () => void;
@@ -40,8 +42,42 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
   const currentState = useSelector((state: RootState) => state.user);
 
   const [firstName, setFirstName] = useState('');
+  const [firstNameValid, setFirstNameValid] = useState(false);
+  const [firstNameTouched, setFirstNameTouched] = useState(false);
+
   const [lastName, setLastName] = useState('');
+  const [lastNameValid, setLastNameValid] = useState(false);
+  const [lastNameTouched, setLastNameTouched] = useState(false);
+
   const [jobPosition, setJobPosition] = useState('');
+  const [jobPositionValid, setJobPositionValid] = useState(false);
+  const [jobPositionTouched, setJobPositionTouched] = useState(false);
+
+  const isValid = (id: string, value: string) => {
+    if (id === 'firstname') {
+      if (value.length > 2) {
+        setFirstNameValid(true);
+      } else {
+        setFirstNameValid(false);
+      }
+    }
+
+    if (id === 'lastname') {
+      if (value.length > 2) {
+        setLastNameValid(true);
+      } else {
+        setLastNameValid(false);
+      }
+    }
+
+    if (id === 'jobposition') {
+      if (value.length > 2) {
+        setJobPositionValid(true);
+      } else {
+        setJobPositionValid(false);
+      }
+    }
+  };
 
   const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -53,9 +89,14 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
       role: currentState.user.role,
       image: '',
     };
-    dispatch(addUser(user));
-    dispatch(isLogin(true));
+
+    if (firstNameValid && lastNameValid && jobPositionValid) {
+      dispatch(addUser(user));
+      dispatch(isLogin(true));
+      onModalCloseHandler();
+    }
   };
+
   const handleFormChange = (event: ChangeEvent) => {
     const target = event.target as HTMLInputElement;
     const value = target.value;
@@ -65,15 +106,21 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
 
     switch (id) {
       case 'firstname':
+        setFirstNameTouched(true);
         setFirstName(value);
+        isValid(id, value);
         break;
 
       case 'lastname':
+        setLastNameTouched(true);
         setLastName(value);
+        isValid(id, value);
         break;
 
       case 'jobposition':
+        setJobPositionTouched(true);
         setJobPosition(value);
+        isValid(id, value);
         break;
 
       default:
@@ -99,6 +146,8 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
           type={USER_INIT.firstName.type}
           label={USER_INIT.firstName.text}
           value={firstName}
+          validate={firstNameValid}
+          touched={firstNameTouched}
           onChangeInputHandler={handleFormChange}
         />
         <Input
@@ -106,6 +155,8 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
           type={USER_INIT.lastName.type}
           label={USER_INIT.lastName.text}
           value={lastName}
+          validate={lastNameValid}
+          touched={lastNameTouched}
           onChangeInputHandler={handleFormChange}
         />
         <Input
@@ -113,6 +164,8 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
           type={USER_INIT.jobPosition.type}
           label={USER_INIT.jobPosition.text}
           value={jobPosition}
+          validate={jobPositionValid}
+          touched={jobPositionTouched}
           onChangeInputHandler={handleFormChange}
         />
         <Input
@@ -122,8 +175,10 @@ const Form: React.FC<IFormProps> = ({ onModalCloseHandler }): JSX.Element => {
           value={''}
           onChangeInputHandler={handleFormChange}
         />
-        <button type="submit">Confirm</button>
-        <Button disabled={false} children={'Cancel'} colorButton="light" onClick={onModalCloseHandler} />
+        <div className={classes.buttons}>
+          <Button disabled={false} children={'Confirm'} colorButton="dark" {...BUTTON_SUBMIT_TYPE} />
+          <Button disabled={false} children={'Cancel'} colorButton="light" onClick={onModalCloseHandler} />
+        </div>
       </form>
     </div>
   );
