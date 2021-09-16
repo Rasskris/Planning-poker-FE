514d6524169863980a9259cf97e6df608ee1968b
Nine-eleven <70841642+Nine-eleven@users.noreply.github.com>
2021-09-16 20:25:19 +0300

    Update pointing-poker/src/App.tsx
    Co-authored-by: Darya Tsimashenkava <70346445+besovadevka@users.noreply.github.com>

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
       <Header />
       <GameSettings />
       <Footer />
-      <TimerContainer timerStarted={true} />
+      <TimerContainer timerStarted />
     </div>
   );
 };
