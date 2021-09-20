e1ef53ca4e481368364d5c5498808e6e0f8b372b
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-20 20:53:19 +0300

    PP-08 feat: add routing

--- pointing-poker/src/App.tsx
+++ pointing-poker/src/App.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import { FC, useEffect, useRef } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import type { Socket } from 'socket.io-client';
+import { initSocket } from './libs';
+import { useAppDispatch, useAppSelector } from './hooks';
+import { selectCurrentUser, selectLoginStatus } from './redux/selectors';
+import { Header, Footer } from './components';
+import { Lobby, Main } from './pages';
 import classes from './App.module.scss';
-import { Header, MainPage, Footer, TimerContainer } from './components';
-import { GameSettings } from './components/GameSettings';
 
-const App: React.FC = () => {
+const App: FC = () => {
+  const socket = useRef<Socket>();
+  const dispatch = useAppDispatch();
+  const isLogin = useAppSelector(selectLoginStatus);
+  const currentUser = useAppSelector(selectCurrentUser);
+
+  const isSocketDissconnect = () => !socket.current || socket.current.disconnected;
+
+  useEffect(() => {
+    if (isSocketDissconnect() && currentUser) {
+      socket.current = initSocket(currentUser.id, currentUser.gameId, dispatch);
+    }
+
+    return () => {
+      if (socket.current?.connected) {
+        socket.current.disconnect();
+      }
+    };
+  });
+
   return (
     <div className={classes.app}>
       <Header />
-      <MainPage />
-      <GameSettings />
+      <Router>
+        <Route exact path="/">
+          {isLogin ? <Redirect to="/lobby" /> : <Main />}
+        </Route>
+        <Route path="/lobby">
+          <Lobby />
+        </Route>
+      </Router>
       <Footer />
-      <TimerContainer timerStarted />
     </div>
   );
 };
