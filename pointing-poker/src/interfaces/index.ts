faecd61c70d0ba8adb9e9a821172e0e15a6cb13a
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-24 02:09:35 +0300

    feat: add new interfaces for Game Settings and Game Round Data

--- pointing-poker/src/interfaces/index.ts
+++ pointing-poker/src/interfaces/index.ts
@@ -2,3 +2,5 @@ export type { User } from './User';
 export type { CurrentUser } from './CurrentUser';
 export type { Message } from './Message';
 export type { Issue } from './Issue';
+export type { IGameSettings } from './IGameSettings';
+export type { IGameRoundData } from './IGameRoundData';
