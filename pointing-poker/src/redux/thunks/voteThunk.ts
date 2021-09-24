385aeb7b29c8069192ce83f8f0b281482d87495b
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-24 13:17:25 +0300

    PP-12-vote feat: add vote thunk

--- pointing-poker/src/redux/thunks/voteThunk.ts
+++ pointing-poker/src/redux/thunks/voteThunk.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { clientAPI } from '../../libs';
 
-export const putVoteForKick = createAsyncThunk('vote/putVote', async (gameId: string) => {
-  const data = await clientAPI.put(`/api/vote`, { gameId });
+interface VoteParams {
+  gameId: string;
+  victimId: string;
+  currentUserId: string;
+}
+
+export const addVote = createAsyncThunk('vote/addVote', async ({ gameId, victimId, currentUserId }: VoteParams) => {
+  const data = await clientAPI.post('/api/vote', { gameId, victimId, currentUserId });
 
   return data;
 });
+
+export const putVoteForKick = createAsyncThunk(
+  'vote/putVote',
+  async ({ gameId, currentUserId }: Partial<VoteParams>) => {
+    const data = await clientAPI.put('/api/vote', { gameId, currentUserId });
+
+    return data;
+  },
+);
