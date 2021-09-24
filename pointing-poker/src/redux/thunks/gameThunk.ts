b21703839543563d36e7e3b5b9b9ccebafbe8226
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-24 13:20:23 +0300

    PP-12-vote feat: add game thunk

--- pointing-poker/src/redux/thunks/gameThunk.ts
+++ pointing-poker/src/redux/thunks/gameThunk.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { clientAPI } from '../../libs';
 
+interface IParams {
+  gameId: string;
+  currentUserId: string;
+  isStarted: boolean;
+}
+
 export const checkExistGame = createAsyncThunk('game/getGame', async (gameId: string) => {
   const data = await clientAPI.get(`/api/games/${gameId}`);
 
   return data;
 });
+
+export const updateGameStatus = createAsyncThunk(
+  'game/updateGameStatus',
+  async ({ gameId, currentUserId, isStarted }: IParams) => {
+    const { status } = await clientAPI.put(`api/games/`, { gameId, currentUserId, isStarted });
+
+    return status;
+  },
+);
