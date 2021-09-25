b3136075290ef2cd18e5437a8f4928ade4409ecc
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-25 23:56:04 +0300

    PP-13 feat: add selectors

--- pointing-poker/src/redux/selectors/index.ts
+++ pointing-poker/src/redux/selectors/index.ts
@@ -3,9 +3,7 @@ import { issuesAdapter, messagesAdapter, usersAdapter } from '../adapters';
 import { createSelector } from 'reselect';
 import { USER_ROLES } from '../../constants';
 
-export const { selectAll: selectIssues, selectById: selectIssueById } = issuesAdapter.getSelectors<RootState>(
-  state => state.issues,
-);
+export const { selectAll: selectIssues } = issuesAdapter.getSelectors<RootState>(state => state.issues);
 
 export const { selectAll: selectMessages } = messagesAdapter.getSelectors<RootState>(state => state.messages);
 
@@ -28,6 +26,10 @@ export const selectVoteStatus = (state: RootState) => state.vote.isActive;
 export const selectVoteVictim = (state: RootState) => state.vote.victim;
 
 export const selectUserOpenedVote = (state: RootState) => state.vote.userOpenedVote;
+
+export const selectScoreValues = (state: RootState) => state.gameSettings.scoreValues;
+
+export const selectScoreTypeShort = (state: RootState) => state.gameSettings.scoreTypeShortSetting;
 
 export const selectDealer = createSelector([selectUsers], users =>
   users.filter(user => user.role === USER_ROLES.DEALER),
