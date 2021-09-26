7804e0d20caa6106d78c3bedcb5033f5c1e44f46
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-27 01:08:12 +0300

    PP-14 feat: update slice in redux for GameRound

--- pointing-poker/src/redux/slices/gameRoundSlice.ts
+++ pointing-poker/src/redux/slices/gameRoundSlice.ts
@@ -1,30 +1,56 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { IGameRoundData } from '../../interfaces';
-import { addGameRoundData, getDataAllRoomsOfGame, updateUserGameCard } from '../thunks';
+import {
+  addGameRoundData,
+  getDataAllRoomsOfGame,
+  getRoundStatistic,
+  updateGameRoundStatistics,
+  updateUserGameCard,
+} from '../thunks';
 
 interface IObjectType {
   [index: string]: string | null;
 }
 
 const initialGameRoundState: IGameRoundData = {
-  timerIsStarted: false,
+  roundIsStarted: false,
   currentIssue: '',
   playerCards: {} as IObjectType,
   roundStatistics: {} as IObjectType,
   isActive: true,
+  //TO DO: add cardTypeValue
 };
 
 export const gameRoundSlice = createSlice({
   name: 'gameRound',
   initialState: initialGameRoundState,
   reducers: {
+    // only used when called by socket
     startGameRound(state, action) {
       const newState = { ...state, ...action.payload };
       return newState;
     },
+    // only used when called by socket
     updateGameRoundData(state, action) {
       const newState = { ...state, ...action.payload };
       return newState;
+    },
+    // only used when called by socket
+    setRoundStatisticFromServer(state, action) {
+      state.roundStatistics = { ...action.payload };
+    },
+    stopGameRound(state) {
+      state.isActive = false;
+      state.roundIsStarted = false;
+    },
+    updateRoundStatistics(state, action) {
+      state.roundStatistics = { ...action.payload };
+    },
+    resetGameRoundData(state) {
+      return { ...initialGameRoundState };
+    },
+    setCurrentIssue(state, action) {
+      state.currentIssue = action.payload;
     },
   },
   extraReducers: builder => {
@@ -37,8 +63,22 @@ export const gameRoundSlice = createSlice({
       return newState;
     });
     builder.addCase(getDataAllRoomsOfGame.fulfilled, (state, action) => {});
+    builder.addCase(updateGameRoundStatistics.fulfilled, state => {
+      return { ...initialGameRoundState };
+    });
+    builder.addCase(getRoundStatistic.fulfilled, (state, action) => {
+      state.roundStatistics = { ...action.payload };
+    });
   },
 });
 
-export const { startGameRound, updateGameRoundData } = gameRoundSlice.actions;
+export const {
+  startGameRound,
+  updateGameRoundData,
+  stopGameRound,
+  updateRoundStatistics,
+  resetGameRoundData,
+  setRoundStatisticFromServer,
+  setCurrentIssue,
+} = gameRoundSlice.actions;
 export const gameRoundReducer = gameRoundSlice.reducer;
