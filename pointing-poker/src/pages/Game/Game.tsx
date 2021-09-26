cfee44fa7d9f158592f749228b0a594346092caa
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-27 01:20:48 +0300

    PP-14 feat: add handlers for buttons, added logic for determining the end of the game, added logic for calculating round statistics, updated rendering of the component

--- pointing-poker/src/pages/Game/Game.tsx
+++ pointing-poker/src/pages/Game/Game.tsx
@@ -1,33 +1,41 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import {
   selectChatStatus,
   selectDealer,
   selectPlayers,
+  selectPlayersAndDealerIds,
+  selectPlayersIds,
   selectUserById,
   selectUserOpenedVote,
   selectVoteStatus,
   selectVoteVictim,
 } from '../../redux/selectors';
-import { addVote, getUsers } from '../../redux/thunks';
+import { addGameRoundData, addVote, getUsers, updateGameRoundStatistics } from '../../redux/thunks';
 import { USER_ROLES } from '../../constants';
 import { IUser } from '../../interfaces';
 import {
+  Button,
   Chat,
   DealerNotification,
   GameCardsList,
   IssueList,
   MemberNotification,
+  TimerContainer,
   UserList,
   VoteNotification,
 } from '../../components';
 import classes from './Game.module.scss';
+import { stopGameRound, updateRoundStatistics } from '../../redux/slices';
+import { IObjectType } from '../../interfaces/IObjectType';
+import { RoundStatistics } from '../../components/RoundStatistics';
 
 interface IGameProps {
   currentUser: IUser;
 }
 
 const Game: FC<IGameProps> = ({ currentUser }) => {
+  const dispatch = useAppDispatch();
   const [victimData, setVictimData] = useState({ id: '', name: '' });
   const isVoteActive = useAppSelector(selectVoteStatus);
   const victim = useAppSelector(selectVoteVictim);
@@ -38,7 +46,11 @@ const Game: FC<IGameProps> = ({ currentUser }) => {
   const players = useAppSelector(selectPlayers);
   const isScoreVisible = true;
   const { id: currentUserId, role: currentUserRole, gameId } = currentUser;
-  const dispatch = useAppDispatch();
+  const settings = useAppSelector(state => state.gameSettings);
+  const gameRoundData = useAppSelector(state => state.gameRound);
+  const playersWhithoutDillerIds = useAppSelector(selectPlayersIds);
+  const playersAndDillerIds = useAppSelector(selectPlayersAndDealerIds);
+  const allPlayersIds = settings.scramMasterAsPlayerSetting ? playersAndDillerIds : playersWhithoutDillerIds;
 
   useEffect(() => {
     dispatch(getUsers(gameId));
@@ -52,48 +64,160 @@ const Game: FC<IGameProps> = ({ currentUser }) => {
     }
   };
 
+  const handlerRestartRound = () => {
+    dispatch(
+      addGameRoundData({
+        gameId: gameId,
+        currentIssue: gameRoundData.currentIssue,
+        playerCards: allPlayersIds,
+        userId: currentUserId,
+      }),
+    );
+  };
+
+  const handlerStartRound = () => {
+    dispatch(
+      addGameRoundData({
+        gameId: gameId,
+        currentIssue: gameRoundData.currentIssue,
+        playerCards: allPlayersIds,
+        userId: currentUserId,
+      }),
+    );
+  };
+
+  const handlerNextIssue = () => {
+    dispatch(updateGameRoundStatistics({ gameId: gameId, gameRoundData, userId: currentUserId }));
+  };
+
+  const handlerStopGameRound = useCallback(() => {
+    dispatch(stopGameRound());
+  }, [dispatch]);
+
+  const checkingNumberPlayersPlayed = useCallback(() => {
+    let playerCards: IObjectType = gameRoundData.playerCards;
+    if (!gameRoundData.isActive) return;
+    if (settings.automaticFlipCardsSetting) {
+      if (Object.keys(playerCards).length === 0) return;
+      for (let key in playerCards) {
+        if (!playerCards[key]) return;
+      }
+      handlerStopGameRound();
+    }
+  }, [gameRoundData.playerCards, gameRoundData.isActive, settings.automaticFlipCardsSetting, handlerStopGameRound]);
+
+  useEffect(() => {
+    checkingNumberPlayersPlayed();
+  }, [checkingNumberPlayersPlayed]);
+
+  const roundStatiscticsCalculation = useCallback(() => {
+    if (gameRoundData.isActive) return;
+    let playerCards: IObjectType = gameRoundData.playerCards;
+    let roundStatisticsMap: Map<string | null, number> = new Map(); // key: score Card Value, value: number of identical cards
+    let numberPlayersOfRound = 0;
+    for (let key in playerCards) {
+      numberPlayersOfRound += 1;
+      let scoreCardValue = playerCards[key] ? playerCards[key] : 'unknown';
+      let numberOfIdenticalCards = roundStatisticsMap.get(scoreCardValue);
+      if (numberOfIdenticalCards) {
+        roundStatisticsMap.set(scoreCardValue, numberOfIdenticalCards + 1);
+      } else {
+        roundStatisticsMap.set(scoreCardValue, 1);
+      }
+    }
+
+    for (let key of roundStatisticsMap.keys() as any) {
+      let numberOfIdenticalCards = roundStatisticsMap.get(key);
+      if (numberOfIdenticalCards) {
+        let percentageValue = (numberOfIdenticalCards * 100) / numberPlayersOfRound;
+        roundStatisticsMap.set(key, percentageValue);
+      }
+    }
+    let roundStatisticsObj = Object.fromEntries(roundStatisticsMap);
+    dispatch(updateRoundStatistics(roundStatisticsObj));
+  }, [gameRoundData.isActive, gameRoundData.playerCards, dispatch]);
+
+  useEffect(() => {
+    roundStatiscticsCalculation();
+  }, [roundStatiscticsCalculation]);
+
   return (
     <section className={classes.game}>
       <div className={classes.content}>
-        <div className={classes.wrapper}>
-          <UserList
-            isScoreVisible={!isScoreVisible}
-            users={dealer}
-            title="Dealer"
-            currentUserId={currentUserId}
-            handleKickUser={handleKickUser}
-          />
+        <div>
+          <div className={classes.wrapper}>
+            <UserList
+              isScoreVisible={!isScoreVisible}
+              users={dealer}
+              title="Dealer"
+              currentUserId={currentUserId}
+              handleKickUser={handleKickUser}
+            />
+          </div>
+          <div className={classes.wrapper}>
+            <IssueList currentUser={currentUser} />
+          </div>
         </div>
-        <div className={classes.wrapper}>
-          <UserList
-            isScoreVisible={isScoreVisible}
-            users={players}
-            title="Players"
-            currentUserId={currentUserId}
-            handleKickUser={handleKickUser}
-          />
+        <div>
+          <div>
+            {settings.isTimerNeededSetting && (
+              <TimerContainer
+                initialMinute={settings.timerValuesSetting.minutes}
+                initialSeconds={settings.timerValuesSetting.seconds}
+                timerStarted={gameRoundData.roundIsStarted}
+                handlerStopTimer={handlerStopGameRound}
+              />
+            )}
+            {/* condition for displaying buttons */}
+            {currentUserRole === USER_ROLES.DEALER &&
+              (gameRoundData.roundIsStarted === gameRoundData.isActive ? (
+                <div>
+                  <Button
+                    type="button"
+                    text="RESTART ROUND"
+                    colorButton="light"
+                    onClick={handlerRestartRound}
+                    disabled={gameRoundData.roundIsStarted ? true : false}
+                  />
+                  <Button type="button" text="NEXT ISSUE" colorButton="dark" onClick={handlerNextIssue} />
+                </div>
+              ) : (
+                <Button type="button" text="RUN ROUND" colorButton="dark" onClick={handlerStartRound} />
+              ))}
+          </div>
+          <div>
+            <RoundStatistics />
+          </div>
         </div>
-        <div className={classes.wrapper}>
-          <IssueList currentUser={currentUser} />
-        </div>
-        <div className={classes.wrapper}>
-          <GameCardsList />
+        <div>
+          {isChatOpen && (
+            <div className={classes.chat}>
+              <Chat currentUser={currentUser} />
+            </div>
+          )}
+          <DealerNotification currentUserId={currentUserId} victimData={victimData} />
+          {isVoteActive && victim && userNameOpenedVote && (
+            <MemberNotification
+              isVoteActive={isVoteActive}
+              currentUserId={currentUserId}
+              victim={victim}
+              userNameOpenedVote={userNameOpenedVote.firstName}
+            />
+          )}
+          <div className={classes.wrapper}>
+            <UserList
+              isScoreVisible={isScoreVisible}
+              users={players}
+              title="Players"
+              currentUserId={currentUserId}
+              handleKickUser={handleKickUser}
+            />
+          </div>
         </div>
       </div>
-      {isChatOpen && (
-        <div className={classes.chat}>
-          <Chat currentUser={currentUser} />
-        </div>
-      )}
-      <DealerNotification currentUserId={currentUserId} victimData={victimData} />
-      {isVoteActive && victim && userNameOpenedVote && (
-        <MemberNotification
-          isVoteActive={isVoteActive}
-          currentUserId={currentUserId}
-          victim={victim}
-          userNameOpenedVote={userNameOpenedVote.firstName}
-        />
-      )}
+      <div className={classes.wrapper}>
+        <GameCardsList />
+      </div>
       <VoteNotification />
     </section>
   );
