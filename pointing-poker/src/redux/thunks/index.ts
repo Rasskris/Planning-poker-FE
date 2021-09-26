2d61fffc2411b138c6e01bf08984ea036f8f8aa6
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-09-27 01:09:42 +0300

    PP 14 feat: add export in index.ts for new thunks

--- pointing-poker/src/redux/thunks/index.ts
+++ pointing-poker/src/redux/thunks/index.ts
@@ -4,4 +4,10 @@ export { getUsers, addUser, updateUser, deleteUser } from './userThunk';
 export { addVote, putVoteForKick } from './voteThunk';
 export { checkExistGame, updateGameStatus } from './gameThunk';
 export { addGameSettings, getGameSettings } from './gameSettingsThunk';
-export { addGameRoundData, updateUserGameCard, getDataAllRoomsOfGame } from './gameRoundThunk';
+export {
+  addGameRoundData,
+  updateUserGameCard,
+  getDataAllRoomsOfGame,
+  updateGameRoundStatistics,
+  getRoundStatistic,
+} from './gameRoundThunk';
