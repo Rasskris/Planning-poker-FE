2c829f6d7ea9a4506369aee98025f98dd0527431
Rasskris <rassoshenko.kristina@yandex.ru>
2021-09-28 18:50:02 +0300

    PP-18 refactor: modify store for logout

--- pointing-poker/src/redux/store/index.ts
+++ pointing-poker/src/redux/store/index.ts
@@ -1,4 +1,6 @@
+import { AnyAction, combineReducers, Reducer } from 'redux';
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { logout } from '../actions';
 import {
   usersReducer,
   messageReducer,
@@ -12,21 +14,35 @@ import {
   newComerReducer,
 } from '../slices';
 
-export const store = configureStore({
-  reducer: {
-    game: gameReducer,
-    currentUser: currentUserReducer,
-    users: usersReducer,
-    messages: messageReducer,
-    issues: issueReducer,
-    gameSettings: gameSettingsReducer,
-    vote: voteReducer,
-    chat: chatReducer,
-    gameRound: gameRoundReducer,
-    newComers: newComerReducer,
-  },
+const appReducer = combineReducers({
+  game: gameReducer,
+  currentUser: currentUserReducer,
+  users: usersReducer,
+  messages: messageReducer,
+  issues: issueReducer,
+  gameSettings: gameSettingsReducer,
+  vote: voteReducer,
+  chat: chatReducer,
+  gameRound: gameRoundReducer,
+  newComers: newComerReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof appReducer>;
+
+export const rootReducer: Reducer = (state: RootState, action: AnyAction) => {
+  if (action.type === logout.type) {
+    return appReducer(initialRootState, action);
+  }
+  return appReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+export const initialRootState = {
+  ...store.getState(),
+};
+
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
