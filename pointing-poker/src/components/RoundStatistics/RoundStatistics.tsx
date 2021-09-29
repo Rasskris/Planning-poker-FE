d6e1ea8ffa8fea4ef7e37819965c821f02dae52f
an-zaz <an_zazerskaya@mail.ru>
2021-09-30 00:50:07 +0300

    PP-17: change design of game page

--- pointing-poker/src/components/RoundStatistics/RoundStatistics.tsx
+++ pointing-poker/src/components/RoundStatistics/RoundStatistics.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
 import { GameCard } from '..';
 import { useAppSelector } from '../../hooks';
+import classes from './RoundStatistics.module.scss';
 
 const RoundStatistics: FC = () => {
   const { roundStatistics } = useAppSelector(state => state.gameRound);
   const precent: string[] = Object.values(roundStatistics);
   const cardScoreValue = Object.keys(roundStatistics);
   return (
-    <div>
+    <div className={classes.wrapper}>
       {cardScoreValue.map((scoreValue, index) => {
         return (
           <div>
