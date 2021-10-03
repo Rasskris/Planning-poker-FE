0123969aa4bff9a29ccf27729b0b1d32e38e199c
Rasskris <rassoshenko.kristina@yandex.ru>
2021-10-03 16:07:04 +0300

    PP-19-refactor: change game page

--- pointing-poker/src/pages/Game/Game.tsx
+++ pointing-poker/src/pages/Game/Game.tsx
@@ -4,6 +4,8 @@ import {
   selectChatStatus,
   selectCurrentIssue,
   selectDealer,
+  selectDoneIssues,
+  selectObservers,
   selectPlayers,
   selectRoundStatus,
   selectSettings,
@@ -41,6 +43,8 @@ import { getStatistics } from '../../utils';
 import { IUser } from '../../interfaces';
 import { logout } from '../../redux/actions';
 import classes from './Game.module.scss';
+import { NavLink } from 'react-router-dom';
+import { resetSelectedCards } from '../../redux/slices';
 
 interface IGameProps {
   currentUser: IUser;
@@ -56,18 +60,22 @@ const Game: FC<IGameProps> = ({ currentUser }) => {
   const isChatOpen = useAppSelector(selectChatStatus);
   const dealer = useAppSelector(selectDealer);
   const players = useAppSelector(selectPlayers);
+  const observers = useAppSelector(selectObservers);
   const settings = useAppSelector(selectSettings);
   const currentIssue = useAppSelector(selectCurrentIssue);
   const timer = useAppSelector(selectTimer);
   const roundStatus = useAppSelector(selectRoundStatus);
+  const doneIssues = useAppSelector(selectDoneIssues);
 
   const isScoreVisible = !!currentIssue?.isDone && !(roundStatus === ROUND_STATUS.STARTED);
   const { scoreTypeShortSetting, scramMasterAsPlayerSetting } = settings;
   const allPlayers = scramMasterAsPlayerSetting && dealer ? players.concat(dealer) : players;
   const { id: currentUserId, role: currentUserRole, gameId } = currentUser;
   const isDealer = currentUserRole === USER_ROLES.DEALER;
-  const isVisibleGameCards = !isDealer || settings.scramMasterAsPlayerSetting;
+  const isObserver = currentUserRole === USER_ROLES.OBSERVER;
   const isRoundStarted = roundStatus === ROUND_STATUS.STARTED;
+  const isVisibleGameCards = !isDealer || settings.scramMasterAsPlayerSetting;
+  const isDoneIssuesExist = doneIssues.length > 0;
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -82,10 +90,17 @@ const Game: FC<IGameProps> = ({ currentUser }) => {
 
   useEffect(() => {
     if (isDealer && roundStatus === ROUND_STATUS.FINISHED) {
-      const statistics = getStatistics(scoreTypeShortSetting, allPlayers, players.length);
+      const statistics = getStatistics(scoreTypeShortSetting, allPlayers, allPlayers.length);
 
       dispatch(updateDoneIssue({ ...currentIssue, statistics, isDone: true }));
       dispatch({ type: updateRoundStatus.fulfilled.type, payload: ROUND_STATUS.NOT_STARTED });
+      dispatch(
+        updateRoundStatus({
+          gameId,
+          userId: currentUserId,
+          roundStatus: ROUND_STATUS.NOT_STARTED,
+        }),
+      );
     }
   });
 
@@ -114,76 +129,105 @@ const Game: FC<IGameProps> = ({ currentUser }) => {
         roundStatus: ROUND_STATUS.STARTED,
       }),
     );
+    dispatch(resetSelectedCards(scoreTypeShortSetting));
   };
 
   return (
     <section className={classes.game}>
-      <div className={classes.content}>
-        {isDealer && <WaitingList />}
-        {isDealer && (
-          <Button
-            text="Stop Game"
-            colorButton="dark"
-            type="button"
-            onClick={handleStopGame}
-            disabled={isRoundStarted}
-          />
-        )}
-        {!isDealer && <Button text="Exit" colorButton="dark" type="button" onClick={handleExitGame} />}
-        <div className={classes.wrapper}>
-          <p>Dealer</p>
-          {dealer && (
-            <UserCard
-              id={dealer.id}
-              currentUserId={currentUserId}
-              firstName={dealer.firstName}
-              lastName={dealer.lastName}
-              role={dealer.role}
-              jobPosition={dealer.jobPosition}
-              handleKickUser={handleKickUser}
-            />
-          )}
-        </div>
-        <div className={classes.wrapper}>
-          <UserList
-            isScoreVisible={isScoreVisible}
-            users={allPlayers}
-            title="Players"
-            currentUserId={currentUserId}
-            handleKickUser={handleKickUser}
-          />
-        </div>
-        <div className={classes.wrapper}>
-          <IssueList currentUser={currentUser} />
-        </div>
-        {isVisibleGameCards && (
-          <div className={classes.wrapper}>
-            <GameCardsList />
-          </div>
-        )}
-        <Timer minutes={timer.minutes} seconds={timer.seconds} />
-        {isDealer &&
-          (currentIssue?.isDone ? (
-            <div>
+      <div className={classes.container}>
+        <div className={classes.wrapperRow}>
+          <div className={classes.wrapperColumn}>
+            {isDoneIssuesExist && (
+              <NavLink className={classes.link} exact to="/statistics">
+                Statistics
+              </NavLink>
+            )}
+            {!isDealer && <Button text="Exit" colorButton="dark" type="button" onClick={handleExitGame} />}
+            {isDealer && <WaitingList />}
+            {isDealer && (
               <Button
+                text="Stop Game"
+                colorButton="dark"
                 type="button"
-                text="RESTART ROUND"
-                colorButton="light"
-                onClick={handleStartRound}
+                onClick={handleStopGame}
                 disabled={isRoundStarted}
               />
+            )}
+            <div className={classes.wrapper}>
+              <p className={classes.title}>Dealer</p>
+              {dealer && (
+                <UserCard
+                  id={dealer.id}
+                  currentUserId={currentUserId}
+                  firstName={dealer.firstName}
+                  lastName={dealer.lastName}
+                  role={dealer.role}
+                  jobPosition={dealer.jobPosition}
+                  handleKickUser={handleKickUser}
+                />
+              )}
             </div>
-          ) : (
-            <div title={currentIssue ? '' : TOOLTIP_TEXT}>
-              <Button
-                type="button"
-                text="RUN ROUND"
-                colorButton="dark"
-                onClick={handleStartRound}
-                disabled={isRoundStarted || !currentIssue}
+            <div className={classes.wrapper}>
+              <IssueList currentUser={currentUser} />
+            </div>
+            <div className={classes.wrapper}>
+              {isDealer &&
+                (currentIssue?.isDone ? (
+                  <div>
+                    <Button
+                      type="button"
+                      text="RESTART ROUND"
+                      colorButton="light"
+                      onClick={handleStartRound}
+                      disabled={isRoundStarted}
+                    />
+                  </div>
+                ) : (
+                  <div title={currentIssue ? '' : TOOLTIP_TEXT}>
+                    <Button
+                      type="button"
+                      text="RUN ROUND"
+                      colorButton="dark"
+                      onClick={handleStartRound}
+                      disabled={isRoundStarted || !currentIssue}
+                    />
+                  </div>
+                ))}
+            </div>
+            <div className={classes.wrapper}>
+              <Timer minutes={timer.minutes} seconds={timer.seconds} />
+            </div>
+          </div>
+          <div className={classes.wrapperColumn}>
+            <div className={classes.wrapper}>
+              <UserList
+                isScoreVisible={isScoreVisible}
+                isPlayer
+                users={allPlayers}
+                title="Players"
+                currentUserId={currentUserId}
+                handleKickUser={handleKickUser}
               />
             </div>
-          ))}
+            <div className={classes.wrapper}>
+              <UserList
+                isScoreVisible={false}
+                isPlayer={false}
+                users={observers}
+                title="Observers"
+                currentUserId={currentUserId}
+                handleKickUser={handleKickUser}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={classes.wrapperRow}>
+          {!isObserver && isVisibleGameCards && (
+            <div className={classes.wrapper}>
+              <GameCardsList />
+            </div>
+          )}
+        </div>
       </div>
       {isChatOpen && (
         <div className={classes.chat}>
