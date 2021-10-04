3bd033e8c64c4411fc62f895daa5f22fe375977f
Rasskris <rassoshenko.kristina@yandex.ru>
2021-10-04 22:27:28 +0300

    PP-22-fix: fix admit/reject newcomer

--- pointing-poker/src/components/Notification/RejectedToGameNotification.tsx
+++ pointing-poker/src/components/Notification/RejectedToGameNotification.tsx
@@ -1,35 +1,29 @@
 import { FC } from 'react';
 import { useAppDispatch } from '../../hooks';
 import { BackDropModal, Button } from '..';
-import { deleteCurrentUser, resetRejectedStatus } from '../../redux/slices';
-import { REDIRECT_TO_MAIN_TEXT, REJECTED_TO_GAME_TEXT } from '../../constants';
+import { REJECTED_TO_GAME_TEXT } from '../../constants';
+import { logout } from '../../redux/actions';
+import { deleteUser } from '../../redux/thunks';
 import classes from './Notification.module.scss';
 
 interface INotificationProps {
   isVisible: boolean;
+  currentUserId: string;
 }
 
-const RejectedToGameNotification: FC<INotificationProps> = ({ isVisible }) => {
+const RejectedToGameNotification: FC<INotificationProps> = ({ isVisible, currentUserId }) => {
   const dispatch = useAppDispatch();
 
-  const handleClickYes = () => {
-    dispatch(resetRejectedStatus());
-    dispatch(deleteCurrentUser());
-  };
-
-  const handleClickNo = () => {
-    dispatch(resetRejectedStatus());
+  const handleClick = () => {
+    dispatch(deleteUser({ currentUserId }));
+    dispatch(logout());
   };
 
   return (
     <BackDropModal isBackDropOpen={isVisible}>
       <div className={classes.notification}>
         <p>{REJECTED_TO_GAME_TEXT}</p>
-        <p>{REDIRECT_TO_MAIN_TEXT}</p>
-        <div className={classes.btnContainer}>
-          <Button type="button" text="Yes" colorButton="dark" onClick={handleClickYes} />
-          <Button type="button" text="No" colorButton="light" onClick={handleClickNo} />
-        </div>
+        <Button type="button" text="Ok" colorButton="dark" onClick={handleClick} />
       </div>
     </BackDropModal>
   );
