c19eb0d2e54dfb7300099e4a99826a8724f7d041
Rasskris <rassoshenko.kristina@yandex.ru>
2021-12-16 16:34:56 +0300

    PP-30 refactor: change header

--- pointing-poker/src/components/Header/Header.tsx
+++ pointing-poker/src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { ChatButton } from '../index';
 import { useAppSelector } from '../../hooks';
-import { selectLoginStatus } from '../../redux/selectors';
+import { selectChatStatus, selectCurrentUser } from '../../redux/selectors';
+import { Chat } from '../Chat';
 import classes from './Header.module.scss';
 
 const Header: FC = () => {
-  const isLogin = useAppSelector(selectLoginStatus);
+  const isChatOpen = useAppSelector(selectChatStatus);
+  const currentUser = useAppSelector(selectCurrentUser);
 
   return (
     <div className={classes.header}>
@@ -15,8 +17,13 @@ const Header: FC = () => {
         </div>
         <div className={classes.icons}>
           <div className={classes.logo} />
-          {isLogin && <ChatButton />}
+          {currentUser && <ChatButton />}
         </div>
+        {currentUser && isChatOpen && (
+          <div className={classes.chat}>
+            <Chat currentUser={currentUser} />
+          </div>
+        )}
       </div>
     </div>
   );
