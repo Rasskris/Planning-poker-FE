ed55a0c4de542452f2d8b5b181327b20f9e24a80
Rasskris <rassoshenko.kristina@yandex.ru>
2021-12-16 16:28:24 +0300

    PP-30 feat: add download results to pdf

--- pointing-poker/src/pages/Statistics/Statistics.tsx
+++ pointing-poker/src/pages/Statistics/Statistics.tsx
@@ -1,30 +1,22 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
-import { GameCard } from '../../components';
-import { useAppSelector } from '../../hooks';
-import { selectDoneIssues } from '../../redux/selectors';
+import ReactToPrint from 'react-to-print';
+import { Button, IssuesStatistics } from '../../components';
 import classes from './Statistics.module.scss';
 
 const Statistics: FC = () => {
-  const doneIssues = useAppSelector(selectDoneIssues);
+  const componentRef = useRef(null);
+
   return (
-    <section>
+    <section className={classes.statisticsContainer}>
       <NavLink className={classes.link} exact to="/game">
         Back to Game
       </NavLink>
-      {doneIssues.map(({ id, title, statistics }) => (
-        <div key={id} className={classes.wrapper}>
-          <p className={classes.issueTitle}>{title}</p>
-          <div className={classes.cardsContainer}>
-            {statistics?.map(({ scoreType, scoreValue, percent }) => (
-              <div key={scoreValue} className={classes.card}>
-                <GameCard scoreType={scoreType} scoreValue={scoreValue} isCurrent={false} />
-                <p>{percent.toFixed(0)}%</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+      <ReactToPrint
+        trigger={() => <Button text="Download PDF Results" colorButton="dark" type="button"></Button>}
+        content={() => componentRef.current}
+      />
+      <IssuesStatistics ref={componentRef} />
     </section>
   );
 };
