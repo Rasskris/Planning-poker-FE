0d96cd383b52d9b848dcd5bbf6c1c078e9fe737e
Rasskris <rassoshenko.kristina@yandex.ru>
2021-12-16 16:39:35 +0300

    PP-30 refactor: change getStatistics

--- pointing-poker/src/utils/getStatistics.ts
+++ pointing-poker/src/utils/getStatistics.ts
@@ -1,6 +1,6 @@
 import { SCORE_VALUES_FN, SCORE_VALUES_PT, VALUE_UNKNOWN } from '../constants';
-import type { Statistics, MapScoreValues } from './types';
-import { IUser } from '../interfaces';
+import type { Statistics, MapScoreValues } from '../types';
+import { User } from '../interfaces';
 
 const mapScoreValues: MapScoreValues = {
   FN: SCORE_VALUES_FN,
@@ -9,10 +9,10 @@ const mapScoreValues: MapScoreValues = {
 
 const getPersent = (countPlayes: number, countCards: number) => (countCards / countPlayes) * 100;
 
-const getAllSelectedCards = (players: IUser[]) =>
+const getAllSelectedCards = (players: User[]) =>
   players.map(player => (player.selectedCard ? player.selectedCard.scoreValue : VALUE_UNKNOWN));
 
-export const getStatistics = (scoreType: string, players: IUser[], countPlayes: number) => {
+export const getStatistics = (scoreType: string, players: User[], countPlayes: number) => {
   const statistics: Statistics = {};
   const scoreValues = mapScoreValues[scoreType];
   const allSelectedCards = getAllSelectedCards(players);
